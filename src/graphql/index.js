ee5f5837614ebf5b7d3fdc5724d093b16666c803
DevNebulae <gamerkeeperson+github@gmail.com>
2017-12-12 14:07:26 +0100

    Add resolvers for data types
    Album resolvers are not working yet.

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -10,6 +10,66 @@ const createSchema = () =>
         albums: (root, args, { models }) => models.Album.findAll(),
         artists: (root, args, { models }) => models.Artist.findAll(),
         tracks: (root, args, { models }) => models.Track.findAll()
+      },
+      Album: {
+        artists: (parent, args, { models }) =>
+          models.Album.findAll({
+            include: [
+              {
+                model: models.Artist,
+                where: { id: parent.id }
+              }
+            ]
+          }),
+        tracks: (parent, args, { models }) =>
+          models.Album.findAll({
+            include: [
+              {
+                model: models.Track,
+                where: { id: parent.id }
+              }
+            ]
+          })
+      },
+      Artist: {
+        tracks: (parent, args, { models }) =>
+          models.Track.findAll({
+            include: [
+              {
+                model: models.Artist,
+                where: { id: parent.id }
+              }
+            ]
+          }),
+        albums: (parent, args, { models }) =>
+          models.Album.findAll({
+            include: [
+              {
+                model: models.Artist,
+                where: { id: parent.id }
+              }
+            ]
+          })
+      },
+      Track: {
+        albums: (parent, args, { models }) =>
+          models.Album.findAll({
+            include: [
+              {
+                model: models.Track,
+                where: { id: parent.id }
+              }
+            ]
+          }),
+        artists: (parent, args, { models }) =>
+          models.Artist.findAll({
+            include: [
+              {
+                model: models.Track,
+                where: { id: parent.id }
+              }
+            ]
+          })
       }
     }
   })
