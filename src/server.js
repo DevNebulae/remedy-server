1100fc03df7fb92221c91103d7b59428727cade3
DevNebulae <gamerkeeperson+github@gmail.com>
2017-12-29 22:12:07 +0100

    Add CORS if the server is run in development mode

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import Koa from "koa"
 import KoaRouter from "koa-router"
 import createSchema from "./graphql"
 import koaBody from "koa-bodyparser"
+import koaCors from "@koa/cors"
 import { graphiqlKoa, graphqlKoa } from "graphql-server-koa"
 import { createModels, linkModels, sequelize } from "./database"
 
@@ -23,8 +24,10 @@ export default async () => {
   router.post("/graphql", koaBody(), graphql)
   router.get("/graphql", graphql)
 
-  if (process.env.NODE_ENV === "development")
+  if (process.env.NODE_ENV === "development") {
     router.get("/graphiql", graphiqlKoa({ endpointURL: "/graphql" }))
+    app.use(koaCors())
+  }
 
   app.use(router.routes())
   app.use(router.allowedMethods())
